9a7efff06ac049a78dfcc74ee9771e52d093a238
Gu Junchao <e056614@mastercard.com>
2015-07-26 11:17:06 +0800

    change db connection

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,8 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'postgresqlServer'
+    // connection: 'postgresqlServer'
+    connection: 'HerokuPsqlServer'
   }
 
 };
